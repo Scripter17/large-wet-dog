ddedb783ece68a5186d28bc241bcbab7a6d6d2d8
James C. Wise <jameschristopherwise@gmail.com>
2026-08-30 11:45:44 -0400

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 //! A discord bot that listens for twitter embed fails and replies to them with fixupx.com links.
 
 use std::borrow::Cow;
-use std::collections::HashMap;
+use std::collections::{HashMap, hash_map::Entry};
 use std::sync::Arc;
 
 use thiserror::Error;
@@ -45,7 +45,7 @@ async fn main() -> Result<(), LwdError> {
 /// A dog that is large and wet.
 #[derive(Debug, Default)]
 pub struct LargeWetDog {
-    pub replies: tokio::sync::RwLock<HashMap<MessageId, MessageId>>,
+    pub replies: tokio::sync::Mutex<HashMap<MessageId, (Option<Timestamp>, MessageId)>>,
 }
 
 #[serenity::async_trait]
@@ -71,8 +71,8 @@ impl EventHandler for LargeWetDog {
                     msg = Cow::Owned(context.http.get_message(msg.channel_id, msg.id).await.expect("The message to still exist"));
                 }
 
-                if let Some(ret) = self.gen_reply(&msg) {
-                    self.replies.write().await.insert(msg.id, msg.reply(&context.http, ret).await.expect("Sending the reply to work").id);
+                if let Some(ret) = self.gen_reply(&msg) && let Entry::Vacant(entry) = self.replies.lock().await.entry(msg.id) {
+                    entry.insert((msg.edited_timestamp, msg.reply(&context.http, ret).await.expect("Sending the reply to work").id));
                 }
             },
             FullEvent::MessageUpdate {event: MessageUpdateEvent {message: msg, ..}, ..} => {
@@ -88,23 +88,25 @@ impl EventHandler for LargeWetDog {
                     msg = Cow::Owned(context.http.get_message(msg.channel_id, msg.id).await.expect("The message to still exist"));
                 }
 
-                if let Some(ret) = self.gen_reply(&msg) {
-                    if let Some(&reply_id) = self.replies.read().await.get(&msg.id) {
-                        context.http.edit_message(msg.channel_id, reply_id, &EditMessage::new().content(ret).allowed_mentions(CreateAllowedMentions::new().replied_user(false)), vec![]).await.expect("The reply message to still exist");
-                    } else {
-                        self.replies.write().await.insert(msg.id, msg.reply(&context.http, ret).await.expect("Sending the reply to work").id);
-                    }
-                } else if let Some(reply_id) = self.replies.write().await.remove(&msg.id) {
-                    context.http.delete_message(msg.channel_id, reply_id, Some("Replied-to message removed failed embeds")).await.expect("Deleting the reply to work");
+                match self.replies.lock().await.entry(msg.id) {
+                    Entry::Occupied(mut entry) => if entry.get().0 < msg.edited_timestamp {
+                        match self.gen_reply(&msg) {
+                            Some(ret) => {entry.insert((msg.edited_timestamp, context.http.edit_message(msg.channel_id, entry.get().1, &EditMessage::new().content(ret).allowed_mentions(CreateAllowedMentions::new().replied_user(false)), vec![]).await.expect("The reply message to still exist").id));},
+                            None      => context.http.delete_message(msg.channel_id, entry.remove().1, Some("Replied-to message removed failed embeds")).await.expect("Deleting the reply to work"),
+                        }
+                    },
+                    Entry::Vacant(entry) => if let Some(ret) = self.gen_reply(&msg) {
+                        entry.insert((msg.edited_timestamp, msg.reply(&context.http, ret).await.expect("Sending the reply to work").id));
+                    },
                 }
             },
-            FullEvent::MessageDelete {channel_id, deleted_message_id, ..} => if let Some(reply_id) = self.replies.write().await.remove(deleted_message_id) {
+            FullEvent::MessageDelete {channel_id, deleted_message_id, ..} => if let Some((_, reply_id)) = self.replies.lock().await.remove(deleted_message_id) {
                 context.http.delete_message(*channel_id, reply_id, Some("Replied-to message deleted")).await.expect("Deleting the reply to work");
             },
             FullEvent::MessageDeleteBulk {channel_id, multiple_deleted_messages_ids, ..} => {
-                let reply_ids = self.replies.write().await.extract_if(|k, _| multiple_deleted_messages_ids.contains(k)).map(|(_, v)| v).collect::<Vec<_>>();
+                let replies = self.replies.lock().await.extract_if(|k, _| multiple_deleted_messages_ids.contains(k)).map(|(_, v)| v).collect::<Vec<_>>();
 
-                for reply_id in reply_ids {
+                for (_, reply_id) in replies {
                     context.http.delete_message(*channel_id, reply_id, Some("Replied-to message deleted")).await.expect("Deleting the reply to work");
                 }
             },
--- src/reply.rs
+++ src/reply.rs
@@ -4,20 +4,18 @@ use std::borrow::Cow;
 use std::fmt::Write;
 use std::sync::LazyLock;
 
-use regex::Regex;
+use regex::{Regex, RegexBuilder};
 use serenity::all::*;
 
 use super::LargeWetDog;
 
 /// The [`Regex`] to check if we should care about a twitter URL.
-static TWITTER: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^https?://(?:www\.)?(?:x|twitter)\.com/([^/]+/status/\d+)" ).expect("???"));
+static TWITTER: LazyLock<Regex> = LazyLock::new(|| RegexBuilder::new(r"^https?://(?:www\.)?(?:x|twitter)\.com/([^/]+/status/\d+)" ).case_insensitive(true).build().expect("???"));
 /// The [`Regex`] to check if we should care about a tunblr URL.
-static TUMBLR : LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^https?://(?:www\.|([^.]+)\.)?tumblr\.com/((?:[^/]+/)?\d+)").expect("???"));
+static TUMBLR : LazyLock<Regex> = LazyLock::new(|| RegexBuilder::new(r"^https?://(?:www\.|([^.]+)\.)?tumblr\.com/((?:[^/]+/)?\d+)").case_insensitive(true).build().expect("???"));
 
 /// The description for present-but-failed twitter embeds.
-const TWITTER_FAIL: Option<&str> = Some("Age\\-restricted adult content\\. This content might not be appropriate for people under 18 years old\\. To view this media, you’ll need to log in to X\\. Learn more");
-/// The description for present-but-failed tumblr embeds.
-const TUMBLR_FAIL: Option<&str> = Some("Tumblr is a place to express yourself, discover yourself, and bond over the stuff you love. It's where your interests connect you with your people.");
+const TWITTER_FAIL_DESC: Option<&str> = Some("Age\\-restricted adult content\\. This content might not be appropriate for people under 18 years old\\. To view this media, you’ll need to log in to X\\. Learn more");
 
 /// Extract a twitter URL's key.
 fn twitter(url: &str) -> Option<Cow<'_, str>> {
@@ -60,12 +58,11 @@ impl LargeWetDog {
         let mut matches = super::get_urls::get_urls(&msg.content).filter_map(get).collect::<Vec<_>>();
 
         let not_fail_twitters = msg.embeds.iter()
-            .filter    (|embed| embed.description.as_deref() != TWITTER_FAIL)
-            .filter    (|embed| embed.image.as_ref().is_some_and(|image| !image.url.contains("/media-preview/") && !image.url.contains("/tweet_video_thumb/")))
+            .filter    (|embed| embed.description.as_deref() != TWITTER_FAIL_DESC && embed.image.as_ref().is_none_or(|image| !image.url.contains("/media-preview/") && !image.url.contains("/tweet_video_thumb/")))
             .filter_map(|embed| twitter(embed.url.as_deref()?));
 
         let not_fail_tumblrs = msg.embeds.iter()
-            .filter(|embed| embed.description.as_deref() != TUMBLR_FAIL)
+            .filter(|embed| embed.timestamp.is_some())
             .filter_map(|embed| tumblr(embed.url.as_deref()?));
 
         for not_fail_twitter in not_fail_twitters {matches.retain(|x| !(x.0 == Category::Twitter && x.1 == not_fail_twitter));}
